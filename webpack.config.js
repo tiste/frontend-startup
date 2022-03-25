e98f0abe481ecccf345abfa427179fb8d7d18fc0
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2022-03-25 17:26:29 +0100

    chore: update template

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,42 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    target: "web",
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].bundle.js",
-    },
-    devServer: {
-        inline: true,
-        port: 3001,
-        watchContentBase: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                ],
+  target: "web",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'src'),
+    open: true,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
             },
+          },
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "css/app.css",
-        }),
-        new HtmlWebpackPlugin({
-            template: "src/index.html",
-        }),
+      },
     ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/app.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
 };
